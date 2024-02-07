8129553c5e095599d875d399168d218db3b7c379
KaMeHb-UA <blade.whiteblack@gmail.com>
2024-02-07 11:16:03 +0200

    support for container multiplication via docker compose

--- start.sh
+++ start.sh
@@ -9,13 +9,17 @@ if [ "$EUID" = '0' ]; then
     exit 0
 fi
 
-mkdir -p /data/data
-mkdir -p /data/.work
+container_number=`docker ps --filter "id=$HOSTNAME" --format '{{.Label "com.docker.compose.container-number"}}'`
+
+if [ "0${container_number}" = '0' ]; then
+    container_number='1'
+fi
+
+mkdir -p /data/${container_number}/data
+mkdir -p /data/${container_number}/.work
 mkdir -p /home/docker/workdir
 
-fuse-overlayfs -o lowerdir=/home/docker/actions-runner,upperdir=/data/data,workdir=/data/.work /home/docker/workdir
-
-sleep 1 # a hack to wait for workdir become alive
+fuse-overlayfs -o lowerdir=/home/docker/actions-runner,upperdir=/data/${container_number}/data,workdir=/data/${container_number}/.work /home/docker/workdir
 
 cd /home/docker/workdir
 
